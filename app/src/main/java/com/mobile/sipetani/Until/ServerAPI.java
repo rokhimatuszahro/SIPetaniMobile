9b19d291a71aed971e068406ee7d94462d69a75d
rokhimatuszahro <57609574+rokhimatuszahro@users.noreply.github.com>
2020-05-21 01:28:42 +0700

    regis
    daftar akun

---
+++ app/src/main/java/com/mobile/sipetani/Until/ServerAPI.java
@@ -0,0 +1,11 @@
+package com.mobile.sipetani.Until;
+
+public class ServerAPI {
+    public static final String URL_REGISTRASI = "http://192.168.43.178/sipetani/ApiMobile/apiRegistrasi";
+    public static final String URL_LOGIN = "http://192.168.43.178/sipetani/ApiMobile/apiLogin";
+    public static final String URL_RESET_AKUN = "http://192.168.43.178/sipetani/ApiMobile/apiResetAkun";
+    public static final String URL_PEMESANAN = "http://192.168.43.178/sipetani/ApiMobile/apiPemesan";
+    public static final String URL_DETAIL_PEMESANAN = "http://192.168.43.178/sipetani/ApiMobile/apiDetailPemesanan";
+    public static final String URL_UPLOAD_BUKTI = "http://192.168.43.178/sipetani/ApiMobile/apiUploadBukti";
+}
+
